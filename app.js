748d147bba1823193ead1ed64a1ef248a8ce24c7
spandananraj634 <83412339+spandananraj634@users.noreply.github.com>
2024-07-13 12:46:09 +0530

    app.js

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 // Iteration 1: Making the play button in the index.html functional.
+document.getElementById("play-button").addEventListener("click", () => {
+    window.location.href = "game.html";
+  });
 // Description: When the play button is clicked the game.html page should be opened
